061ffbc9efe32e75a9dce6603e7843738ba136a0
Bea Malsky <beamalsky@gmail.com>
2018-11-07 15:13:53 -0600

    Clean up urls

--- committeeoversightapp/urls.py
+++ committeeoversightapp/urls.py
@@ -6,6 +6,4 @@ urlpatterns = [
     url(r'accounts/', include('django.contrib.auth.urls')),
     url(r'^$', EventCreate.as_view(), name='create-event'),
     url(r'^list$', EventList.as_view(), name='list-event'),
-    # url(r'^view/(?P<pk>\d+)$', EventView.as_view(), name="view-event"),
-    # url(r'^success/$', Success.as_view(), name='success'),
 ]
