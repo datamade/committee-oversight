364c2ab3f0128e417e248b07461e62d4f1c63143
Bea Malsky <beamalsky@gmail.com>
2018-12-04 14:05:18 -0600

    Slice in template actually

--- committeeoversightapp/views.py
+++ committeeoversightapp/views.py
@@ -103,7 +103,6 @@ class EventList(LoginRequiredMixin, ListView):
     model = Event
     template_name = 'list.html'
     context_object_name = 'hearings'
-    queryset = Event.objects.all().order_by('-created_at')[:500]
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
