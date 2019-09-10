956f0159ffde8798b31248d9fb27dbcb71c79b51
Bea Malsky <beamalsky@gmail.com>
2019-09-10 13:18:32 -0500

    Start of category pages

--- committeeoversightapp/models.py
+++ committeeoversightapp/models.py
@@ -1,6 +1,17 @@
 from django.db import models
+import re
+
+from wagtail.core.models import Page
+from wagtail.core.fields import StreamField
+from wagtail.core import blocks
+from wagtail.admin.edit_handlers import StreamFieldPanel, MultiFieldPanel
+from wagtail.images.edit_handlers import ImageChooserPanel
+from wagtail.images.blocks import ImageChooserBlock
+
 from opencivicdata.core.models import Organization
-from opencivicdata.legislative.models import Event, EventParticipant, EventDocument
+from opencivicdata.legislative.models import Event, EventParticipant, \
+                                             EventDocument
+
 
 class HearingCategoryType(models.Model):
     id = models.CharField(max_length=100, primary_key=True)
@@ -9,17 +20,65 @@ class HearingCategoryType(models.Model):
     def __str__(self):
         return u'({0}) {1}'.format(self.id, self.name)
 
+
 class HearingCategory(models.Model):
     event = models.ForeignKey(Event, null=True, on_delete=models.CASCADE)
-    category = models.ForeignKey(HearingCategoryType, null=True, on_delete=models.CASCADE)
+    category = models.ForeignKey(HearingCategoryType,
+                                 null=True,
+                                 on_delete=models.CASCADE)
+
 
 class WitnessDetails(models.Model):
     witness = models.ForeignKey(EventParticipant, on_delete=models.CASCADE)
-    document = models.ForeignKey(EventDocument, null=True, blank=True, on_delete=models.CASCADE)
-    organization = models.CharField(max_length=100, null=True, blank=True, primary_key=False)
+    document = models.ForeignKey(EventDocument,
+                                 null=True,
+                                 blank=True,
+                                 on_delete=models.CASCADE)
+    organization = models.CharField(max_length=100,
+                                    null=True,
+                                    blank=True,
+                                    primary_key=False)
     retired = models.BooleanField(default=False)
+
 
 class Committee(models.Model):
     lugar_id = models.IntegerField(null=True, blank=True, primary_key=False)
-    lugar_name = models.CharField(max_length=200, null=True, blank=True, primary_key=False)
-    organization = models.ForeignKey(Organization, null=True, blank=True, on_delete=models.CASCADE)
+    lugar_name = models.CharField(max_length=200,
+                                  null=True,
+                                  blank=True,
+                                  primary_key=False)
+    organization = models.ForeignKey(Organization,
+                                     null=True,
+                                     blank=True,
+                                     on_delete=models.CASCADE)
+
+
+class CategoryDetailPage(Page):
+    body = StreamField([
+        ('heading', blocks.CharBlock(classname='full title', icon='openquote')),
+        ('paragraph', blocks.RichTextBlock()),
+        ('image', ImageChooserBlock()),
+        ('button',
+         blocks.StructBlock([
+            ('button_text', blocks.CharBlock()),
+            ('button_link', blocks.URLBlock())
+            ],
+         icon='site'))
+    ])
+
+    # Editor configuration
+    content_panels = Page.content_panels + [
+        StreamFieldPanel('body'),
+    ]
+    promote_panels = [
+        MultiFieldPanel(Page.promote_panels, "Page configuration"),
+    ]
+
+    def get_context(self, request):
+        context = super().get_context(request)
+        path = context['request'].path
+        pk = re.search(r'/(\d+)/', path).group(1)
+
+        context['object'] = HearingCategoryType.objects.get(id=pk)
+
+        return context
