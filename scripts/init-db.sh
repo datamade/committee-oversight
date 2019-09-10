b71ba94edabc9bc9fe6e20606f369ac3509d5a03
Bea Malsky <beamalsky@gmail.com>
2019-09-10 13:34:04 -0500

    Remove init-db.sh

--- scripts/init-db.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-psql -U postgres -c "CREATE DATABASE hearings"
-psql -U postgres -d hearings -c "CREATE EXTENSION IF NOT EXISTS postgis"
-# Add any more database initialization commands you may need here
