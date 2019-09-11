71119196a5c4adbca3af8944cb695c93411e2c60
Bea Malsky <beamalsky@gmail.com>
2019-09-11 17:23:09 -0500

    Use create hearing page as landing for now

--- committeeoversightapp/urls.py
+++ committeeoversightapp/urls.py
@@ -4,7 +4,7 @@ from committeeoversightapp.views import EventCreate, EventList, EventDelete, \
 
 urlpatterns = [
     url(r'accounts/', include('django.contrib.auth.urls')),
-    url(r'^hearing/create/$', EventCreate.as_view(), name='create-event'),
+    url(r'^$', EventCreate.as_view(), name='create-event'),
     url(r'^hearing/list$', EventList.as_view(), name='list-event'),
     url(r'^hearing/delete/(?P<pk>.*?/.*?)/', EventDelete.as_view(), name="delete-event"),
     url(r'^hearing/edit/(?P<pk>.*?/.*?)/', EventEdit.as_view(), name="edit-event"),
