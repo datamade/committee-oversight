c84fac24f2eab6a7fa455f2af87108a08138c9f5
Bea Malsky <beamalsky@gmail.com>
2019-10-09 14:43:55 -0500

    Add error handling for missing site

--- committeeoversightapp/templatetags/cms_tags.py
+++ committeeoversightapp/templatetags/cms_tags.py
@@ -2,11 +2,16 @@ from django import template
 
 register = template.Library()
 
+from wagtail.core.models import Site
+
 @register.simple_tag(takes_context=True)
 def get_site_root(context):
-    # NB this returns a core.Page, not the implementation-specific model used
-    # so object-comparison to self will return false as objects would differ
-    return context['request'].site.root_page
+    # No site is returned in the request for local previews of custom models.
+    # To get around this bug, we use the first Site object, assuming there is only one
+    try:
+        return context['request'].site.root_page
+    except AttributeError:
+        return Site.objects.first().root_page
 
 
 def has_menu_children(page):
