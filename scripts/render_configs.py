429fb585c8d2a773dff7260bf61ea4b245e8e215
Bea Malsky <beamalsky@datamade.us>
2020-02-14 11:52:37 -0600

    Add committee ratings crontask

--- scripts/render_configs.py
+++ scripts/render_configs.py
@@ -19,9 +19,13 @@ if __name__ == "__main__":
   nginx_template_path = '/home/datamade/committee-oversight-{0}/configs/committee-oversight-{1}.conf.nginx'.format(
     deployment_id, deployment_group)
   nginx_outpath = '/etc/nginx/conf.d/committee-oversight.conf'
+
   supervisor_template_path = '/home/datamade/committee-oversight-{0}/configs/committee-oversight-{1}.conf.supervisor'.format(
     deployment_id, deployment_group)
   supervisor_outpath = '/etc/supervisor/conf.d/committee-oversight.conf'
+
+  crontask_template_path = '/home/datamade/committee-oversight-{0}/scripts/committee-oversight-crontasks'.format(deployment_id)
+  crontask_outpath = '/etc/cron.d/committee-oversight-crontasks'
 
   with open(nginx_template_path) as f:
     nginx_conf = Template(f.read())
@@ -36,8 +40,16 @@ if __name__ == "__main__":
     supervisor_rendered = supervisor_conf.render(
       {'deployment_id': deployment_id})
 
+  with open(crontask_template_path) as f:
+    crontask_conf = Template(f.read())
+    contask_rendered = crontask_conf.render(
+      {'deployment_id': deployment_id})
+
   with open(nginx_outpath, 'w') as out:
     out.write(nginx_rendered)
 
   with open(supervisor_outpath, 'w') as out:
     out.write(supervisor_rendered)
+
+  with open(crontask_outpath, 'w') as out:
+    out.write(contask_rendered)
